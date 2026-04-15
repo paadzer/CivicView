297301ed67803f6921b4d02c0ccc3c1e59a82bb8
paadzer <patrickcd03@gmail.com>
2026-04-15 20:54:57 +0100

    pushing

---
+++ civicview/__init__.py
@@ -0,0 +1,5 @@
+## civicview/__init__.py
+## This file is used to initialize the civicview app.
+## It is used to create the app and to register the app with the Django project.
+## It is also used to create the app and to register the app with the Django project.
+## It is also used to create the app and to register the app with the Django project.
